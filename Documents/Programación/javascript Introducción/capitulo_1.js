3b62c6a5e8c625b496f0e1227a8ac4063175adaa
jsmlz1 <juansebas199533@gmail.com>
2024-03-13 15:52:55 -0500

    javascript Introducción

---
+++ Documents/Programación/javascript Introducción/capitulo_1.js
@@ -0,0 +1,13 @@
+//bindings
+/*
+esto es un comentario
+*/
+let total = 0, count = 1;
+while (count <= 10) {
+  total += count;
+  count += 1;
+}
+console.log(total);
+// → 55
+console.log(suma(rango(1, 10)));
+// → 55
