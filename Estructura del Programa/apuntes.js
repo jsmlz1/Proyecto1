ff505129a281ddcf1ae45d5cbfa5f6823d18fa22
jsmlz1 <juansebas199533@gmail.com>
2024-03-15 18:49:18 -0500

    Se agrega la carpeta Se agrega la carpeta introducción

---
+++ Estructura del Programa/apuntes.js
@@ -0,0 +1,48 @@
+//Expresiones y declaraciones
+
+1;
+!false;
+
+//Bindings
+
+let caught = 5 * 5;
+
+let ten = 10;
+console.log(ten * ten);
+// → 100
+
+let mood = "light";
+console.log(mood);
+// → light
+mood = "dark";
+console.log(mood);
+// → dark
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+// → 105
+
+let one = 1, two = 2;
+console.log(one + two);
+// → 3
+
+var name = "Ayda";
+const greeting = "¡Hola ";
+console.log(greeting + name);
+// → ¡Hola Ayda
+
+//Funciones
+
+prompt("Ingrese el código de acceso");
+
+//palabras clave y palabras reservadas
+
+"break case catch class const continue debugger default"
+"delete do else enum export extends false finally for"
+"function if implements import interface in instanceof let"
+"new package private protected public return static super"
+"switch this throw true try typeof var void while with yield"
+
+//La función console.log
+
